1df8fbcca1bcc6def0d4f0464e18c5c38b4d4486
Niraj-123456 <lamaniraj321@gmail.com>
2021-07-24 19:29:57 +0545

    navbar items added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,55 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
-      <h2>This is a Header</h2>
+    <div class="header">
+      <nav class="nav-container">
+        <a class="logo" href="#">
+          Niraj Lama
+        </a>
+        <div class="main-links">
+          <ul class="nav-links">
+            <li>
+              <a class="link-item" href="#">
+                My Work
+              </a>
+            </li>
+            <li>
+              <a class="link-item" href="#">
+                About Me
+              </a>
+            </li>
+            <li>
+              <a class="link-item" href="#">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="social-links">
+          <ul class="nav-links">
+            <li>
+              <a className="link-item">
+                <img src="images/facebook-circular-logo.png" alt="Facebook" />
+              </a>
+            </li>
+            <li>
+              <a className="link-item">
+                <img src="images/instagram.svg" alt="Instagram" />
+              </a>
+            </li>
+            <li>
+              <a className="link-item">
+                <img src="images/twitter.png" alt="Twitter" />
+              </a>
+            </li>
+            <li>
+              <a className="link-item">
+                <img src="images/github.svg" alt="Github" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
