3c633502e1be2e9bda82daa6afa6538cb30345ab
Niraj-123456 <lamaniraj321@gmail.com>
2021-08-14 21:12:57 +0545

    more projecct and footer section added

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div class="copyright">copyright &copy;2021</div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,38 @@
 import React from "react";
 
-function projects() {
-  return <div>This is project section</div>;
+function Projects() {
+  return (
+    <div class="projects">
+      <div class="heading">More of my projects</div>
+      <div class="project-section">
+        <div class="project-one">
+          <h1>My first javascript app</h1>
+          <p>
+            Sunt id eiusmod mollit dolor fugiat deserunt. Anim aute officia
+            minim qui et velit ullamco reprehenderit duis velit ullamco. Aliqua
+            dolore in nostrud consectetur ex proident nostrud sit id
+            consectetur.
+          </p>
+          <img src="images/javascript_screenshot.png" alt="javascript-app" />
+        </div>
+        <div class="project-two">
+          <h1>Disney Plus Clone</h1>
+          <p>
+            Sunt id eiusmod mollit dolor fugiat deserunt. Anim aute officia
+            minim qui et velit ullamco reprehenderit duis velit ullamco. Aliqua
+            dolore in nostrud consectetur ex proident nostrud sit id
+            consectetur.
+          </p>
+          <img
+            src="images/disney_plus_clone_screenshot.png"
+            alt="disney-plus-clone"
+          />
+        </div>
+      </div>
+
+      <div className="view-more">View More...</div>
+    </div>
+  );
 }
 
-export default projects;
+export default Projects;
