ac31d15d51c20fb2196915b2ecb77b228f2e534a
Niraj-123456 <lamaniraj321@gmail.com>
2021-08-14 20:16:57 +0545

    skills section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Featured from "./components/Featured";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Featured />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/projects.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function projects() {
+  return <div>This is project section</div>;
+}
+
+export default projects;
