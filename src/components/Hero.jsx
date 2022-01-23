fc6ac441bec97facaf7d51e1f3e1ef8d79a8b586
Niraj-123456 <lamaniraj321@gmail.com>
2022-01-23 21:55:06 +0545

    hero section style added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,21 +6,9 @@ function Hero() {
       <div className="hero-text">
         <h1>Wanna be Professional Front-end Developer</h1>
         <p>Hi, I'm Niraj Lama. I am Looking for React Js Internship</p>
-        <div className="scroll-icon">
-          <svg width="18" height="26" viewBox="0 0 18 26" fill="none">
-            <rect
-              x="1.5"
-              y="1.5"
-              width="15"
-              height="23"
-              rx="7.5"
-              fill="#6B18DA"
-              stroke="white"
-              strokeWidth="3"
-            />
-            <ellipse cx="9" cy="9.86209" rx="4" ry="3.58621" fill="white" />
-          </svg>
-        </div>
+      </div>
+      <div className="hero-image">
+        <img src="../../public/images/avatar_1.png" alt="avatar" />
       </div>
     </div>
   );
