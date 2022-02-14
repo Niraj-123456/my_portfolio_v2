30d59515a42879246a9104d07f908c55790a3491
Niraj Lama <lamaniraj321@gmail.com>
2022-02-14 22:09:02 +0545

    about me page added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function About() {
-  return <main className="home-wrapper">This is about me section</main>;
+  return (
+    <main className="home-wrapper">
+      <div className="about-wrapper">
+        <div className="about-content">
+          <h1>About Me</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio
+            corrupti expedita fuga quis explicabo minus at pariatur ex deleniti.
+            Similique, impedit. Ipsum, labore officia laborum rem consequatur
+            iusto esse nam! Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. <br />
+            <br />
+            Odio corrupti expedita fuga quis explicabo minus at pariatur ex
+            deleniti. Similique, impedit. Ipsum, labore officia laborum rem
+            consequatur iusto esse nam! Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. <br /> <br />
+            Odio corrupti expedita fuga quis explicabo minus at pariatur ex
+            deleniti. Similique, impedit. Ipsum, labore officia laborum rem
+            consequatur iusto esse nam!
+          </p>
+        </div>
+        <div className="about-img">
+          <img src="images/about_me.png" alt="About_Me_Img" />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default About;
