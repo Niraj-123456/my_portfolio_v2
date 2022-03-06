351816b9f1cfe720f0b5686bf34a6dc765f976d7
nirajlama <lamaniraj321@gmail.com>
2022-03-06 13:02:25 +0545

    merge conflict solved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
-=======
 import Hero from "./components/Hero";
 import Featured from "./components/Featured";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
->>>>>>> 3c633502e1be2e9bda82daa6afa6538cb30345ab
 
 function App() {
   return (
