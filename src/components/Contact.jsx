4e66c5bd23e67b7d4cbdbe3deb25f48f210c742f
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2022-04-13 22:14:03 +0545

    use formik for handling contact from

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 function Contact() {
-  const [contact, setContact] = useState({
+  const initialValues = {
     fullname: "",
     email: "",
     phone: "",
     address: "",
     message: "",
+  };
+
+  const contactFormValidationSchema = Yup.object().shape({
+    fullname: Yup.string().required("Your Full Name is required!"),
+    email: Yup.string()
+      .email("Invalid email address!")
+      .required("Your Email is required!"),
+    phone: Yup.string().required("Your Phone Number is required!"),
+    address: Yup.string().required("Your address is required!"),
+    message: Yup.string().required("Please enter your message!"),
   });
 
-  const handleChange = (e) => {
-    setContact({ ...contact, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Thank you for reaching out to me.");
-  };
   return (
     <main className="home-wrapper">
       <div className="contact">
@@ -30,55 +34,106 @@ function Contact() {
           <img src="images/contact_bg.jpg" alt="Get In Touch" />
         </div>
         <div className="contact-form">
-          <form onSubmit={handleSubmit}>
-            <h2>Send a message</h2>
-            <label htmlFor="fullname">Full Name</label>
-            <input
-              required
-              type="text"
-              name="fullname"
-              id="fullname"
-              value={contact.fullname}
-              onChange={handleChange}
-            />
-            <label htmlFor="email">E-mail</label>
-            <input
-              required
-              type="email"
-              name="email"
-              id="email"
-              value={contact.email}
-              onChange={handleChange}
-            />
-            <label htmlFor="phone">Phone Number</label>
-            <input
-              required
-              type="text"
-              name="phone"
-              id="phone"
-              value={contact.phone}
-              onChange={handleChange}
-            />
-            <label htmlFor="address">Address</label>
-            <input
-              required
-              type="text"
-              name="address"
-              id="address"
-              value={contact.address}
-              onChange={handleChange}
-            />
-            <label htmlFor="message">Message</label>
-            <textarea
-              rows="5"
-              type="text"
-              name="message"
-              id="message"
-              value={contact.message}
-              onChange={handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={contactFormValidationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log(values);
+              setSubmitting(false);
+            }}
+          >
+            {({
+              dirty,
+              values,
+              errors,
+              touched,
+              isSubmitting,
+              isValid,
+              handleChange,
+              handleBlur,
+            }) => (
+              <Form className="form">
+                <h2>Send a message</h2>
+                <label htmlFor="fullname">Full Name</label>
+                <input
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  value={values.fullname}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.fullname && touched.fullname ? (
+                  <div className="error__msg">{errors.fullname}</div>
+                ) : (
+                  ""
+                )}
+                <label htmlFor="email">E-mail</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email ? (
+                  <div className="error__msg">{errors.email}</div>
+                ) : (
+                  ""
+                )}
+                <label htmlFor="phone">Phone Number</label>
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phone && touched.phone ? (
+                  <div className="error__msg">{errors.phone}</div>
+                ) : (
+                  ""
+                )}
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  id="address"
+                  value={values.address}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.address && touched.address ? (
+                  <div className="error__msg">{errors.address}</div>
+                ) : (
+                  ""
+                )}
+                <label htmlFor="message">Message</label>
+                <textarea
+                  rows="5"
+                  type="text"
+                  name="message"
+                  id="message"
+                  value={values.message}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.message && touched.message ? (
+                  <div className="error__msg">{errors.message}</div>
+                ) : (
+                  ""
+                )}
+                <button
+                  type="submit"
+                  disabled={isSubmitting || !isValid || !dirty}
+                >
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </main>
