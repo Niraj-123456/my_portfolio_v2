785ab265a521098a33b469182b607cde17b5f4bb
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2022-09-25 19:52:51 +0545

    few changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
@@ -12,20 +7,18 @@ import Footer from "./components/Footer/Footer";
 import MyWork from "./components/MyWork/MyWork";
 import Contact from "./components/Contact/Contact";
 import About from "./components/About/About";
-// import NotFound from "./components/NotFound/NotFound";
 import { projectData } from "./data";
 
 function App() {
   return (
-    <Router basename="/my_portfolio_v2">
+    <Router>
       <div className="app-container">
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<MyWork data={projectData} />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/aboutme" element={<About />} />
-          <Route index path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Footer />
       </div>
