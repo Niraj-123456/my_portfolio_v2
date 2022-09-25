41fcbcda21f1f1f538101792298070516e42e835
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2022-09-25 09:56:42 +0545

    initial commit

---
+++ src/components/common/ScrollIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function ScrollIcon() {
+  return (
+    <div className="scroll-icon">
+      <svg width="18" height="26" viewBox="0 0 18 26" fill="none">
+        <rect
+          x="1.5"
+          y="1.5"
+          width="15"
+          height="23"
+          rx="7.5"
+          fill="#6B18DA"
+          stroke="white"
+          strokeWidth="3"
+        />
+        <ellipse cx="9" cy="9.86209" rx="4" ry="3.58621" fill="white" />
+      </svg>
+    </div>
+  );
+}
+
+export default ScrollIcon;
