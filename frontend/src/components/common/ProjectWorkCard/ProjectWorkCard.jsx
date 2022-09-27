98b57b110ee889aebb9c93eb49e5038b117ce8ba
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2022-09-27 22:24:40 +0545

    addedd project links

--- frontend/src/components/common/ProjectWorkCard/ProjectWorkCard.jsx
+++ frontend/src/components/common/ProjectWorkCard/ProjectWorkCard.jsx
@@ -16,11 +16,13 @@ function ProjectWorkCard({ project }) {
           <h1 className="card__title">{project.name}</h1>
           <p className="card__description">{project.description}</p>
           <div className="card__button">
-            <Button
-              label="View"
-              style={{ color: "var(--color-pink)" }}
-              onClick={handleClick}
-            />
+            <a href={project.githubLink}>
+              <Button
+                label="View"
+                style={{ color: "var(--color-pink)" }}
+                onClick={handleClick}
+              />
+            </a>
           </div>
         </div>
       </div>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -18,27 +18,41 @@ export const projectData = [
     name: "Connect Bazaar",
     description:
       "Connect Bazaar is an e-commerce start-up for all types of goods and services. Next Js has been implemented on the front-end and Nest Js on the backend. The unique thing about the project is the story viewer component where seller can post their products in the form of stories for the customer to view.",
-    githubLink: "/",
+    githubLink: "#",
   },
   {
     image: "images/disney_plus_clone.png",
     name: "Choose My Basket",
     description:
       "Choose My Basket is an e-commerce site for daily based goods and services. As a front-end developer, I have implemented React Js for front-end along with tailwind css for styling.",
-    githubLink: "/",
+    githubLink: "#",
   },
   {
     image: "images/sajilo-jiwan.png",
     name: "Sajilo Jiwan",
     description:
       "Sajilo Jiwan is a home-based service platform which is a start-up based in Nepal. My major role on the project was to work on the front-end implementation using Next Js.",
-    githubLink: "/",
+    githubLink: "#",
   },
   {
     image: "images/chat-app.png",
     name: "Chat App",
     description:
       "This is a simple chat application built with reactjs on the front-end and nodejs on the backend. For chat implementation socket-io is used on back-end and socket-io-client is used in front-end. ",
-    githubLink: "/",
+    githubLink: "#",
+  },
+  {
+    image: "images/chat-app.png",
+    name: "Weather App",
+    description:
+      "This is a weather application built with reactjs on the front-end integrated with free weather api for loading weather data. The users can enter a location for which they want the weather data.",
+    githubLink: "https://Niraj-123456.github.io/weather-app",
+  },
+  {
+    image: "images/chat-app.png",
+    name: "Movie App",
+    description:
+      "This is a simple movie application built with reactjs on the front-end and nodejs on the backend. The user can view list of available movies, can add new movie and also delete a movie. The application also has sorting and searching features for movie. ",
+    githubLink: "https://github.com/Niraj-123456/movieapp",
   },
 ];
