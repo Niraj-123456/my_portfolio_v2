befa1ccc77502f550a3e35faaafd6ce95ef2a565
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2022-09-27 20:42:52 +0545

    project github link added

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,41 +4,41 @@ export const projectData = [
     name: "Disney Plus Clone",
     description:
       "This is my early React Js project when I was still learning React. I did the project watching a video tutorial. The project has been built with React Js on the front-end and Firebase on the back-end. The project is a clone of Disney Plus- an OTT platform. The project has bunch of movie lists where user can click on the movie and view the movie details. Auth system has also been maintain using Firebase auth system.",
-    link: "/",
+    githubLink: "https://github.com/Niraj-123456/disney-plus-clone",
   },
   {
     image: "images/social_media_app_screenshot.JPG",
     name: "Blog App",
     description:
       "It is a basic blog app built with React Js and Firebase with features like adding, editing and deleting blogs by the user. Users can register and log in to the system. Authentication has been implemented using the firebase default auth. ",
-    link: "/",
+    githubLink: "https://github.com/Niraj-123456/my-socialmedia-app",
   },
   {
     image: "images/connect-bazaar.png",
     name: "Connect Bazaar",
     description:
       "Connect Bazaar is an e-commerce start-up for all types of goods and services. Next Js has been implemented on the front-end and Nest Js on the backend. The unique thing about the project is the story viewer component where seller can post their products in the form of stories for the customer to view.",
-    link: "/",
+    githubLink: "/",
   },
   {
     image: "images/disney_plus_clone.png",
     name: "Choose My Basket",
     description:
       "Choose My Basket is an e-commerce site for daily based goods and services. As a front-end developer, I have implemented React Js for front-end along with tailwind css for styling.",
-    link: "/",
+    githubLink: "/",
   },
   {
     image: "images/sajilo-jiwan.png",
     name: "Sajilo Jiwan",
     description:
       "Sajilo Jiwan is a home-based service platform which is a start-up based in Nepal. My major role on the project was to work on the front-end implementation using Next Js.",
-    link: "/",
+    githubLink: "/",
   },
   {
     image: "images/chat-app.png",
     name: "Chat App",
     description:
       "This is a simple chat application built with reactjs on the front-end and nodejs on the backend. For chat implementation socket-io is used on back-end and socket-io-client is used in front-end. ",
-    link: "/",
+    githubLink: "/",
   },
 ];
